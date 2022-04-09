46a656052e7d151b9ef9967da1adf9e411ab7858
seef-elshnawy <66653332+seef-elshnawy@users.noreply.github.com>
2022-04-09 03:48:30 +0200

    Add files via upload

---
+++ seef-elshnawy-E-shop-backend-main/‏‏e_shop1/ecommarce/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+urlpatterns=[
+    path('users',views.getUsers,name='users'),
+    path('user',views.addUser,name='user'),
+    path('updateuser/<str:pk>',views.updateUser,name='updateuser'),
+    path('deleteuser/<str:pk>',views.deleteUser),
+    path('login',views.Login),
+    path('products',views.getProducts,name='products'),
+    path('product/<str:pk>',views.getProduct),
+    path('addproduct',views.addProducts),
+    path('updateproduct/<str:pk>',views.updateProduct),
+    path('deleteproduct/<str:pk>',views.deleteProduct)
+]
