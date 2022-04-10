0f48689872ae2c99ab6a81ed6ab1c9c2846f505b
seef-elshnawy <66653332+seef-elshnawy@users.noreply.github.com>
2022-04-10 03:57:00 +0200

    Update urls.py

--- e_shop-main/e_shop/urls.py
+++ e_shop-main/e_shop/urls.py
@@ -19,6 +19,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('server/',include('ecommarce.urls'))
+    path('server/',include('ecommarce.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
