8770381821d59031216c6db1afab2f76f038fde4
seef-elshnawy <66653332+seef-elshnawy@users.noreply.github.com>
2022-04-14 01:03:58 +0200

    Update serializer.py

--- e_shop-main/ecommarce/serializer.py
+++ e_shop-main/ecommarce/serializer.py
@@ -3,7 +3,8 @@ from rest_framework import serializers
 from .models import Users,Products
 
 class log(serializers.ModelSerializer):
-    request.data._mutable= True
+    def create(request,*args,**kwargs):
+      request.data._mutable= True
     class Meta:
       model=(Users)
       fields='__all__'
