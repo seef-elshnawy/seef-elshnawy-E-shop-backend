2c30c6a9f4007aa8c46cb9e79c61b333bc581513
seef-elshnawy <66653332+seef-elshnawy@users.noreply.github.com>
2022-04-15 00:36:15 +0200

    Update urls.py

--- e_shop-main/ecommarce/urls.py
+++ e_shop-main/ecommarce/urls.py
@@ -5,6 +5,7 @@ urlpatterns=[
     path('user',views.addUser,name='user'),
     path('updateuser/<str:pk>',views.updateUser,name='updateuser'),
     path('deleteuser/<str:pk>',views.deleteUser),
+    path('resetpassword/<str:pk>',views.Resetpassword),
     path('login',views.Login),
     path('products',views.getProducts,name='products'),
     path('product/<str:pk>',views.getProduct),
